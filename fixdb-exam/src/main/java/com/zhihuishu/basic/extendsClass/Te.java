67c0134a0ca5770a668ffde00a00779510da0ea9
xiazhongwei <xiazhongwei@able-elec.com>
2018-07-05 22:47:13 +0800

    juc

---
+++ fixdb-exam/src/main/java/com/zhihuishu/basic/extendsClass/Te.java
@@ -0,0 +1,7 @@
+package com.zhihuishu.basic.extendsClass;
+
+/**
+ * Created by Administrator on 2018/4/30.
+ */
+public class Te {
+}
---
+++ fixdb-exam/src/main/java/com/zhihuishu/junit/proxy/staticProxy/IUserDao.java
@@ -0,0 +1,8 @@
+package com.zhihuishu.junit.proxy.staticProxy;
+
+/**
+ * Created by Administrator on 2018/4/30.
+ */
+public interface IUserDao {
+    void save();
+}

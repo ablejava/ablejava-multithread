b47889c83418658765cebbfe6966cbd9f75cd896
xiazhongwei <xiazhongwei@able-elec.com>
2018-07-07 19:17:44 +0800

    juc learning

---
+++ fixdb-exam/src/main/java/com/zhihuishu/thread/join/AddThread.java
@@ -0,0 +1,28 @@
+package com.zhihuishu.thread.join;
+
+/**
+ * Created by Administrator on 2018/7/7.
+ */
+public class AddThread extends Thread{
+
+    public volatile static int i =0;
+
+    @Override
+    public void run() {
+        for (i =0; i< 1000000; i++);
+    }
+
+
+    /**
+     * 主线程会等待子线程执行的结果
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException{
+        AddThread at = new AddThread();
+        at.start();
+        at.join();
+        System.out.println(i);
+    }
+
+}
--- fixdb-exam/src/main/java/com/zhihuishu/thread/join/Run.java
+++ fixdb-exam/src/main/java/com/zhihuishu/thread/join/Run.java
@@ -21,4 +21,7 @@ public class Run {
             e.printStackTrace();
         }
     }
+
+
+
 }

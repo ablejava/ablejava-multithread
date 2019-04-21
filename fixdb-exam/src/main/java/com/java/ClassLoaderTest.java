209bf6df500c92eced223da55dd544482ff2533c
ablejava <1298243794@qq.com>
2019-04-21 11:01:57 +0800

    classloader

---
+++ fixdb-exam/src/main/java/com/java/ClassLoaderTest.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.List;
+
+public class ClassLoaderTest {
+
+    static {
+        System.out.println("test");
+    }
+    public static void main(String[] args) throws Exception{
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        Class<?> aClass = classLoader.loadClass("com.java.ClassLoaderTest");
+        System.out.println("--------------------------------");
+        aClass = Class.forName("com.java.ClassLoaderTest");
+    }
+}
